2f3bcb2eda178845495075f5c877430628c184d3
jtomas <jtomas@upc.es>
2023-12-02 10:56:09 +0100

    APARTADO 8 Sensor

--- app/src/main/java/com/example/tomas/examen/MainActivity.java
+++ app/src/main/java/com/example/tomas/examen/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.tomas.examen;
 import static com.example.tomas.examen.Registro.contarUsuariosConectados;
 import static com.example.tomas.examen.Registro.crearMapDesdeListas;
 
+import static java.lang.Math.abs;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,29 +24,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener { //APARTADO 8 Sensor
 
+    //APARTADO 3 Java
     List<String> USUARIO = Arrays.asList("ajuan", "pmati", "pmati", "ajuan", "pmati");
     List<Integer> MINUTO = Arrays.asList(1, 3, 4, 5, 8);
     List<String> TIPO = Arrays.asList("E", "E", "S", "S", "E");
-
     //APARTADO 6
-    static Map<Integer, Registro> registros; // Lo hacemos static para poder acceder a él desde RecyclerViewActivity
+    public static Map<Integer, Registro> registros; // Lo hacemos static para poder acceder a él desde RecyclerViewActivity
+    //APARTADO 8 Sensor
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //APARTADO 3.b)
+        //APARTADO 3.b) Java
         registros = crearMapDesdeListas(USUARIO, MINUTO, TIPO);
         Log.d("Registro", "Registros: " + registros);
 
-        //APARTADO 4.a)
+        //APARTADO 4.a) Algoritmo
         int usuariosConectados = contarUsuariosConectados(registros);
         Log.d("Registro", "Usuarios conectados: " + usuariosConectados);
 
-        //APARTADO 4.b)
+        //APARTADO 4.b) Algoritmo
         //No es necesario crear un método. LO hacemos directamente.
         Map<String, Integer> tiempoPorUsuario = new HashMap<>();
         Map<String, Integer> ultimoIngreso = new HashMap<>();
@@ -64,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         });
         Log.d("Registro", "Tiempo por usuario: " + tiempoPorUsuario);
 
-        //APARTADO 5
+        //APARTADO 5 Actividad
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //APARTADO 6
+        //APARTADO 6 RecyclerView
         Button button2 = findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,4 +99,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    //APARTADO 8 Sensor
+    @Override
+    public void onResume() {
+        super.onResume();
+        //APARTADO 8 Sensor
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> listaSensores = sensorManager.getSensorList(Sensor.TYPE_MAGNETIC_FIELD);
+        if (!listaSensores.isEmpty()) {
+            Sensor magneticSensor = listaSensores.get(0);
+            sensorManager.registerListener(this, magneticSensor, SensorManager.SENSOR_STATUS_ACCURACY_HIGH);}
+    }
+
+    @Override
+    public void onPause() {
+        sensorManager.unregisterListener(this);
+        super.onPause();
+    }
+
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        double lectura= sensorEvent.values[0];
+        Log.d("SENSORES", "Magnetico en x: "+lectura);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {}
 }
